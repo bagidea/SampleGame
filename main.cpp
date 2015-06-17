48ccdabf672cd7d161ba4fffb0b1223c1d39348b
bagidea <aodbboy@hotmail.com>
2015-06-17 15:26:32 +0700

    Update Main for Use

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include "GameWindow.h"
 #include "GameTexture.h"
 
-GameWindow bis;
-GameTexture image;
+GameWindow* bis;
+GameTexture* image;
 
 int speed;
 
@@ -10,33 +10,44 @@ void Start()
 {
 	speed = 1;
 
-	image.SetRenderer(bis.GetRenderer());
-	if(!image.Load("source/Bomb.png"))
+	image = new GameTexture();
+	image->SetRenderer(bis->GetRenderer());
+	if(!image->Load("source/Bomb.png"))
 	{
 		cerr << "Load source/Bomb.png Fail" << endl;
 	}
 
-	image.SetWidth(50);
-	image.SetHeight(50);
+	image->SetWidth(50);
+	image->SetHeight(50);
 }
 
 void Update()
 {
-	image.SetX(image.GetX()+speed);
-	image.SetY(image.GetY()+speed);
-	image.SetWidth(image.GetWidth()+speed);
-	image.SetHeight(image.GetHeight()+speed);
-	image.Render();
+	image->SetX(image->GetX()+speed);
+	image->SetY(image->GetY()+speed);
+	image->SetWidth(image->GetWidth()+speed);
+	image->SetHeight(image->GetHeight()+speed);
+	image->Render();
+}
+
+void Close()
+{
+	delete image;
+	delete bis;
 }
 
 int main(int argc, char* argv[])
 {
-	if(!bis.CreateWindow("Sample Game", 800, 600))
+	bis = new GameWindow();
+
+	if(!bis->CreateWindow("Sample Game", 800, 600))
 	{
 		cerr << "Can't Create GameWindow" << endl;
 	}else{
-		bis.Run((void*)&Start, (void*)Update);
+		bis->Run((void*)&Start, (void*)Update);
 	}
+
+	Close();
 
 	return 0;
 }
